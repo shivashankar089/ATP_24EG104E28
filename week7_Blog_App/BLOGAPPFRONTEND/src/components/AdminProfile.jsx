f79d87ad07c1d9c670f8bd1e2b56e216f04197e8
shivashankar089 <shivashankar0889@gmail.com>
2026-04-13 23:12:30 +0530

    week7_Blog_App

---
+++ week7_Blog_App/BLOGAPPFRONTEND/src/components/AdminProfile.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function AdminProfie(){
+    return(
+        <div></div>
+    )
+}
+
+export default AdminProfie;
---
+++ week7_Blog_App/BLOGAPPFRONTEND/src/components/AuthorsList.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function AuthorList(){
+    return(
+        <div></div>
+    )
+}
+
+export default AuthorList;
