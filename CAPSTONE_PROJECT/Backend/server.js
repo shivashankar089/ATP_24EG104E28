2dacfe4d900772f7b96c45404c7abc4279c39446
Nukala Shiva Shankar <shivashankar0889@gmail.com>
2026-05-12 13:27:56 +0530

    Commit for Deployement

--- CAPSTONE_PROJECT/Backend/server.js
+++ CAPSTONE_PROJECT/Backend/server.js
@@ -30,10 +30,12 @@ app.use("/auth", commonApp);
 const connectDB = async () => {
   try {
     await connect(process.env.DB_URL);
-
+    console.log("Database connected successfully");
     //assign port
     const port = process.env.PORT || 5000;
-    app.listen(port, () => {});
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
   } catch (err) {
     console.error("Database connection failed:", err);
   }
@@ -46,7 +48,9 @@ app.use((req, res, next) => {
 
   res.status(404).json({ message: `path ${req.url} is invalid` });
 });
-
+app.get("/", (req, res) => {
+    res.send("Backend is running");
+});
 //Error handling middleware
 app.use((err, req, res, next) => {
 
