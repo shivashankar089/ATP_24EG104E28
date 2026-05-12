67490320d665d14e845ddfcec17d419c96852d5e
Nukala Shiva Shankar <shivashankar0889@gmail.com>
2026-05-12 13:31:02 +0530

    Commit for Deployement

--- CAPSTONE_PROJECT/Backend/server.js
+++ CAPSTONE_PROJECT/Backend/server.js
@@ -32,7 +32,7 @@ const connectDB = async () => {
     await connect(process.env.DB_URL);
     console.log("Database connected successfully");
     //assign port
-    const port = process.env.PORT || 5000;
+    const port = process.env.PORT || 4000;
     app.listen(port, () => {
       console.log(`Server is running on port ${port}`);
     });
